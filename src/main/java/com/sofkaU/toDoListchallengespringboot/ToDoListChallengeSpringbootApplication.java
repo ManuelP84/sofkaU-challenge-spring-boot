179b6f1acf610b6ad4b84cf49b1b4547a5a1e51e
Manuel Pineda <pineda.manuel@gmail.com>
2022-05-04 16:44:50 -0500

    Start project ---- Start to-do-app project. Initial commit

---
+++ src/main/java/com/sofkaU/toDoListchallengespringboot/ToDoListChallengeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.sofkaU.toDoListchallengespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListChallengeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListChallengeSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofkaU/toDoListchallengespringboot/ToDoListChallengeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofkaU.toDoListchallengespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListChallengeSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
