c05dd031332603e7cc1a4e1512d548bde5590088
Manuel Pineda <pineda.manuel@gmail.com>
2022-05-05 03:10:34 -0500

    Service ---- Service layer implementation

---
+++ src/main/java/com/sofkaU/toDoListchallengespringboot/service/CategoryNoteService.java
@@ -0,0 +1,40 @@
+package com.sofkaU.toDoListchallengespringboot.service;
+
+import com.sofkaU.toDoListchallengespringboot.dao.CategoryNoteDaoImplement;
+import com.sofkaU.toDoListchallengespringboot.entity.Category;
+import com.sofkaU.toDoListchallengespringboot.entity.Note;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryNoteService {
+
+    @Autowired
+    private CategoryNoteDaoImplement categoryNoteService;
+
+    public List<Category> getCategories(){
+        return categoryNoteService.getCategories();
+    }
+
+    public Category createCategory(Category category){
+        return categoryNoteService.createCategory(category);
+    }
+
+    public Category createNote(Note note){
+        return categoryNoteService.createNote(note);
+    }
+
+    public Category updateNote(Note note){
+        return categoryNoteService.updateNote(note);
+    }
+
+    public void deleteNote(Note note){
+        categoryNoteService.deleteNote(note);
+    }
+
+    public void deleteCategory(Category category){
+        categoryNoteService.deleteCategory(category);
+    }
+}
